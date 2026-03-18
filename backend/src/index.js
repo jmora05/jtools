c37040c445169000267dd6fd46da9ae547781574
cristianquinto <118476477+cristianquinto@users.noreply.github.com>
2026-03-18 15:13:50 -0500

    testConnection

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,8 +7,8 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// probar conexión
-//testConnection();
+// probar conexión  
+testConnection(); 
 
 // ================= RUTAS =================
 const empleadosRoutes              = require('./routes/empleadosRoutes.js');
